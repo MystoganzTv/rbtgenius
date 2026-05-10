a27866e0054bc114fc4f5e2618ff431c4786e95c
Enrique <enrique.padron853@gmail.com>
2026-05-10 17:45:20 -0400

    Isolate translation loader from question barrel

--- src/lib/questions/index.js
+++ src/lib/questions/index.js
@@ -1,4 +1,3 @@
 export * from "./question-bank.js";
 export * from "./task-list.js";
 export * from "./concept-task-list-map.js";
-export { initTranslations } from "./question-translations-es-loader.js";
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "@/App.jsx";
 import "@/index.css";
-import { initTranslations } from "@/lib/questions";
+import { initTranslations } from "@/lib/questions/question-translations-es-loader.js";
 
 // Load Spanish question translations — non-blocking, questions show in English until ready
 initTranslations();
